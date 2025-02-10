e01326dd42b1e2f7ea11acf45c7e7bb2885b9d12
Abhishek Rao <abhishekrao5002@gmail.com>
2025-02-09 19:39:55 -0800

    Remove unused db argument in test.

--- backend/app/tests/test_models/test_user.py
+++ backend/app/tests/test_models/test_user.py
@@ -1,18 +1,18 @@
-import pytest
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.core.security import get_password_hash
+
 
 def test_create_user(db: Session, test_user: dict):
     user = User(
         email=test_user["email"],
         hashed_password=get_password_hash(test_user["password"]),
-        username=test_user["username"]
+        username=test_user["username"],
     )
     db.add(user)
     db.commit()
     db.refresh(user)
-    
+
     assert user.email == test_user["email"]
     assert user.username == test_user["username"]
-    assert user.hashed_password != test_user["password"] 
+    assert user.hashed_password != test_user["password"]
